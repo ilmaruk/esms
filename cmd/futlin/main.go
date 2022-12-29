193e1107cf0bc8bc14a1746ff7a527f591e61144
ilmaruk <ruggero.ferretti@gmail.com>
2022-12-29 15:54:18 +0100

    Full initialisation.

--- cmd/futlin/main.go
+++ cmd/futlin/main.go
@@ -38,7 +38,6 @@ func main() {
 	var teams [2]models.Team
 	var err error
 
-	initTeamsData()
 	teams[0], err = esms.ReadTeam("./bin/foo.txt", "./bin/foosht.txt")
 	if err != nil {
 		panic(err)
@@ -68,7 +67,11 @@ func main() {
 		formalMinute := halfStart
 		for minute := halfStart; minute <= lastMinuteOfHalf; minute++ {
 			cleanInjCardIndicators()
-			recalculateTeamsData()
+			recalculateTeamData(&teams[0], &teams[1], true)
+			recalculateTeamData(&teams[1], &teams[0], false)
+
+			// b, _ := json.Marshal(teams[0])
+			// fmt.Println(string(b))
 
 			// For each team
 			//
@@ -124,12 +127,167 @@ func main() {
 	calcAbility()
 }
 
-func initTeamsData() {
+func cleanInjCardIndicators() {}
 
+func calcAggression(t *models.Team) {
+	t.Aggression = 0
+
+	for i := 0; i < NUM_PLAYERS; i++ {
+		if t.Players[i].Active != 1 {
+			t.Players[i].Ag = 0
+		}
+
+		t.Aggression += t.Players[i].Ag
+	}
 }
 
-func cleanInjCardIndicators()   {}
-func recalculateTeamsData()     {}
+func calcPlayerContributions(p *models.Player) {
+	if p.Active != 1 {
+		p.TkContrib = 0
+		p.PsContrib = 0
+		p.ShContrib = 0
+
+		return
+	}
+
+	// double tk_mult = tact_manager().get_mult(team[a].tactic, team[!a].tactic,
+	// 	team[a].player[b].pos, "TK");
+	// double ps_mult = tact_manager().get_mult(team[a].tactic, team[!a].tactic,
+	// 		team[a].player[b].pos, "PS");
+	// double sh_mult = tact_manager().get_mult(team[a].tactic, team[!a].tactic,
+	// 		team[a].player[b].pos, "SH");
+
+	var tkMult float64 = 1
+	var psMult float64 = 1
+	var shMult float64 = 1
+
+	sideFactor := .75
+	if (p.Side == "R" && p.LikesRight) || (p.Side == "L" && p.LikesLeft) || (p.Side == "C" && p.LikesCenter) {
+		sideFactor = 1.
+	}
+
+	p.TkContrib = tkMult * float64(p.Tk) * sideFactor * p.Fatigue
+	p.PsContrib = psMult * float64(p.Ps) * sideFactor * p.Fatigue
+	p.ShContrib = shMult * float64(p.Sh) * sideFactor * p.Fatigue
+}
+
+// Adjusts players' total contributions, taking into account the
+// side balance on each position
+//
+func adjustContribWithSideBalance(t *models.Team) {
+	// The side balance:
+	// For each position (w/o side), keep a vector of 3 elements
+	// to specify the number of players playing R [0], L [1], C [2] on this position
+	//
+	balance := make(map[string][]int)
+
+	// Init the side balance for all positions
+	//
+	for _, pos := range []string{"DF", "DM", "MF", "AM", "FW"} {
+		balance[pos] = []int{0, 0, 0}
+	}
+
+	// Go over the team's players and record on what side they play,
+	// updating the side balance
+	//
+	for b := 1; b < NUM_PLAYERS; b++ {
+		if t.Players[b].Active == 1 && t.Players[b].Pos != "GK" {
+			switch t.Players[b].Side {
+			case "R":
+				balance[t.Players[b].Pos][0]++
+			case "L":
+				balance[t.Players[b].Pos][1]++
+			case "C":
+				balance[t.Players[b].Pos][2]++
+			}
+		}
+	}
+
+	// For all positions, check if the side balance is equal for R and L
+	// If it isn't, penalize the contributions of the players on those positions
+	//
+	// Additionally, penalize teams who play with more than 3 C players on
+	// some position without R and L
+	//
+	for _, pos := range []string{"DF", "DM", "MF", "AM", "FW"} {
+		onPosRight := balance[pos][0]
+		onPosLeft := balance[pos][0]
+		onPosCenter := balance[pos][0]
+
+		var taxedMultiplier float64 = 1
+
+		if onPosLeft != onPosRight {
+			taxRatio := 0.25 * math.Abs(float64(onPosRight-onPosLeft)) / float64(onPosRight+onPosLeft)
+			taxedMultiplier = 1 - taxRatio
+		} else if onPosLeft == 0 && onPosRight == 0 && onPosCenter > 3 {
+			taxedMultiplier = .87
+		}
+
+		if taxedMultiplier != 1 {
+			for b := 2; b < NUM_PLAYERS; b++ {
+				if t.Players[b].Active == 1 && t.Players[b].Pos == pos {
+					t.Players[b].TkContrib *= taxedMultiplier
+					t.Players[b].PsContrib *= taxedMultiplier
+					t.Players[b].ShContrib *= taxedMultiplier
+				}
+			}
+		}
+	}
+}
+
+func calcTeamContributionsTotal(t *models.Team) {
+	for b := 2; b < NUM_PLAYERS; b++ {
+		t.TeamTackling += t.Players[b].TkContrib
+		t.TeamPassing += t.Players[b].PsContrib
+		t.TeamShooting += t.Players[b].ShContrib
+	}
+}
+
+func calcShotProb(att, def *models.Team, home bool) {
+	// Note: 1.0 is added to tackling, to avoid singularity when the
+	// team tackling is 0
+	//
+	att.ShotProb = 1.8 * (float64(att.Aggression)/50.0 + 800.0*
+		math.Pow(((1.0/3.0*att.TeamShooting+2.0/3.0*att.TeamPassing)/(def.TeamTackling+1.0)), 2))
+
+	// If it is the home team, add home bonus
+	//
+	if home {
+		att.ShotProb += 0 // homeBonus
+	}
+}
+
+func recalculateTeamData(att, def *models.Team, home bool) {
+	att.TeamTackling = 0
+	att.TeamPassing = 0
+	att.TeamShooting = 0
+	calcAggression(att)
+
+	for b := 1; b < NUM_PLAYERS; b++ {
+		if att.Players[b].Active == 1 {
+			fatigueDeduction := att.Players[b].NominalFatiguePerMinute
+			mrnd := rand.Intn(100)
+			fatigueDeduction += float64(mrnd-50) / 50.0 * 0.003
+
+			att.Players[b].Fatigue -= fatigueDeduction
+
+			if att.Players[b].Fatigue < 0.10 {
+				att.Players[b].Fatigue = .10
+			}
+		}
+	}
+
+	for b := 1; b < NUM_PLAYERS; b++ {
+		if b != att.CurrentGk {
+			calcPlayerContributions(&att.Players[b])
+		}
+	}
+
+	adjustContribWithSideBalance(att)
+	calcTeamContributionsTotal(att)
+
+	calcShotProb(att, def, home)
+}
 func ifFoul(w int)              {}
 func randomInjury(w int)        {}
 func checkConditionals(w int)   {}
@@ -164,7 +322,7 @@ func ifShot(att, def models.Team) {
 			// shooter = who_got_assist(a, assister);
 
 			internal.PrintCommentary(os.Stdout, internal.ASSISTEDCHANCE, "[MIN]", att.Name, att.Players[assister].Name, att.Players[shooter].Name)
-			// team[a].player[assister].keypasses++;
+			att.Players[assister].KeyPasses++
 		} else {
 			shooter = whoDidIt(att, DID_SHOT)
 			// chanceAssisted = false
@@ -246,9 +404,9 @@ func ifShot(att, def models.Team) {
 // shooting skill
 //
 func whoDidIt(att models.Team, event int) int {
-	// var total float64 = 0
-	// var weight float64 = 0
-	// var ar []float64 = make([]float64, NUM_PLAYERS+1)
+	var total float64 = 0
+	var weight float64 = 0
+	var ar []float64 = make([]float64, NUM_PLAYERS)
 
 	// Employs the weighted random algorithm
 	// A player's chance to DO_IT is his
@@ -256,34 +414,34 @@ func whoDidIt(att models.Team, event int) int {
 	// contribution
 	//
 
-	// for k := 1; k <= NUM_PLAYERS; k++ {
-	// 	switch event {
-	// 	case DID_SHOT:
-	// 		weight += att.Players[k].ShContrib * 100.0
-	// 		total = att.TeamShooting * 100.0
-	// 		break
-	// 	case DID_FOUL:
-	// 		weight += att.Players[k].Ag
-	// 		total = att.Aggression
-	// 		break
-	// 	case DID_TACKLE:
-	// 		weight += att.Players[k].TkContrib * 100.0
-	// 		total = att.TeamTackling * 100.0
-	// 		break
-	// 	case DID_ASSIST:
-	// 		weight += att.Players[k].PsContrib * 100.0
-	// 		total = att.TeamPassing * 100.0
-	// 		break
-	// 	}
+	for k := 0; k < NUM_PLAYERS; k++ {
+		switch event {
+		case DID_SHOT:
+			weight += att.Players[k].ShContrib * 100.0
+			total = att.TeamShooting * 100.0
+			break
+		case DID_FOUL:
+			weight += float64(att.Players[k].Ag)
+			total = float64(att.Aggression)
+			break
+		case DID_TACKLE:
+			weight += att.Players[k].TkContrib * 100.0
+			total = att.TeamTackling * 100.0
+			break
+		case DID_ASSIST:
+			weight += att.Players[k].PsContrib * 100.0
+			total = att.TeamPassing * 100.0
+			break
+		}
 
-	// 	ar[k] = weight
-	// }
+		ar[k] = weight
+	}
 
-	// randValue := total * rand.Float64()
+	randValue := total * rand.Float64()
 
 	var k int
-	// for k = 2; ar[k] <= randValue; k++ {
-	// }
+	for k = 1; ar[k] <= randValue; k++ {
+	}
 
 	return k
 }
