1c757416b6c5e520ccdd05c98b4f0bd2cc00f15f
ilmaruk <ruggero.ferretti@gmail.com>
2023-11-25 23:30:25 +0100

    Better team burg generation (azgaar)

--- go.mod
+++ go.mod
@@ -2,15 +2,17 @@ module github.com/ilmaruk/esms
 
 go 1.20
 
-require golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
+require (
+	github.com/google/uuid v1.4.0
+	github.com/spf13/viper v1.17.0
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+)
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/google/uuid v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pallinder/go-randomdata v1.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
@@ -18,7 +20,6 @@ require (
 	github.com/spf13/afero v1.10.0 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.17.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
--- internal/logic/generators/team_name.go
+++ internal/logic/generators/team_name.go
@@ -1,38 +1,68 @@
 package generators
 
 import (
+	"reflect"
+	"strings"
+
 	"github.com/ilmaruk/esms/internal/random"
-	"github.com/pallinder/go-randomdata"
 )
 
-var teamNamePrefixes = []string{
-	"Atletico",
-	"Athletic",
-	"Dinamo",
-	"Dynamo",
-	"FC",
-	"Fortuna",
-	"Lokomotiv",
-	"Olimpia",
-	"Olimpija",
-	"Partizan",
-	"Pro",
-	"Racing",
-	"Real",
-	"Sparta",
-	"Spartak",
-	"Sport",
-	"Sporting",
-	"Standard",
-	"Torpedo",
-	"Union",
-	"Universitario",
-	"Viktoria",
-	"Virtus",
-	"Vitoria",
+const (
+	isPrefix int = 1
+	isSuffix int = 2
+)
+
+var teamNamePrefixes = map[string]int{
+	"Atletico":      isPrefix,
+	"Athletic":      isPrefix,
+	"Burg":          isSuffix,
+	"Dinamo":        isPrefix,
+	"Dynamo":        isPrefix,
+	"FC":            isPrefix | isSuffix,
+	"Fortuna":       isPrefix,
+	"Lokomotiv":     isPrefix,
+	"Olimpia":       isPrefix,
+	"Olimpija":      isPrefix,
+	"Partizan":      isPrefix,
+	"Pro":           isPrefix,
+	"Racing":        isPrefix,
+	"Real":          isPrefix,
+	"Sparta":        isPrefix,
+	"Spartak":       isPrefix,
+	"Sport":         isPrefix,
+	"Sporting":      isPrefix,
+	"Standard":      isPrefix,
+	"Torpedo":       isPrefix,
+	"Union":         isPrefix,
+	"United":        isSuffix,
+	"Universitario": isPrefix,
+	"Viktoria":      isPrefix,
+	"Virtus":        isPrefix,
+	"Vitoria":       isPrefix,
 }
 
+var azgaarElvenBurgs = "Adrindest,Aethel,Afranthemar,Aiqua,Alari,Allanar,Almalian,Alora,Alyanasari,Alyelona,Alyran,Ammar,Anyndell,Arasari,Aren,Ashmebel,Aymlume,Bel-Didhel,Brinorion,Caelora,Chaulssad,Chaundra,Cyhmel,Cyrang,Dolarith,Dolonde,Draethe,Dranzan,Draugaust,E'ana,Eahil,Edhil,Eebel,Efranluma,Eld-Sinnocrin,Elelthyr,Ellanalin,Ellena,Ellorthond,Eltaesi,Elunore,Emyranserine,Entheas,Eriargond,Esari,Esath,Eserius,Eshsalin,Eshthalas,Evraland,Faellenor,Famelenora,Filranlean,Filsaqua,Gafetheas,Gaf Serine,Geliene,Gondorwin,Guallu,Haeth,Hanluna,Haulssad,Heloriath,Himlarien,Himliene,Hinnead,Hlinas,Hloireenil,Hluihei,Hlurthei,Hlynead,Iaenarion,Iaron,Illanathaes,Illfanora,Imlarlon,Imyse,Imyvelian,Inferius,Inlurth,innsshe,Iralserin,Irethtalos,Irholona,Ishal,Ishlashara,Ithelion,Ithlin,Iulil,Jaal,Jamkadi,Kaalume,Kaansera,Karanthanil,Karnosea,Kasethyr,Keatheas,Kelsya,Keth Aiqua,Kmlon,Kyathlenor,Kyhasera,Lahetheas,Lefdorei,Lelhamelle,Lilean,Lindeenil,Lindoress,Litys,Llaughei,Lya,Lyfa,Lylharion,Lynathalas,Machei,Masenoris,Mathethil,Mathentheas,Meethalas,Menyamar,Mithlonde,Mytha,Mythsemelle,Mythsthas,Naahona,Nalore,Nandeedil,Nasad Ilaurth,Nasin,Nathemar,Neadar,Neilon,Nelalon,Nellean,Nelnetaesi,Nilenathyr,Nionande,Nylm,Nytenanas,Nythanlenor,O'anlenora,Obeth,Ofaenathyr,Ollmnaes,Ollsmel,Olwen,Olyaneas,Omanalon,Onelion,Onelond,Orlormel,Ormrion,Oshana,Oshvamel,Raethei,Rauguall,Reisera,Reslenora,Ryanasera,Rymaserin,Sahnor,Saselune,Sel-Zedraazin,Selananor,Sellerion,Selmaluma,Shaeras,Shemnas,Shemserin,Sheosari,Sileltalos,Siriande,Siriathil,Srannor,Sshanntyr,Sshaulu,Syholume,Sylharius,Sylranbel,Taesi,Thalor,Tharenlon,Thelethlune,Thelhohil,Themar,Thene,Thilfalean,Thilnaenor,Thvethalas,Thylathlond,Tiregul,Tlauven,Tlindhe,Ulal,Ullve,Ulmetheas,Ulssin,Umnalin,Umye,Umyheserine,Unanneas,Unarith,Undraeth,Unysarion,Vel-Shonidor,Venas,Vin Argor,Wasrion,Wlalean,Yaeluma,Yeelume,Yethrion,Ymserine,Yueghed,Yuerran,Yuethin"
+
 func GenerateTeamName(rnd random.Randomiser) string {
-	prefix := teamNamePrefixes[rnd.UniformRandom(len(teamNamePrefixes))]
-	return prefix + " " + randomdata.City()
+	// Burg
+	burgs := strings.Split(azgaarElvenBurgs, ",")
+	burg := burgs[rnd.UniformRandom(len(burgs))]
+
+	// Prefix/Suffix
+	keys := reflect.ValueOf(teamNamePrefixes).MapKeys()
+	value := keys[rnd.UniformRandom(len(keys))].Interface().(string)
+
+	prefixSuffix := teamNamePrefixes[value]
+	isP := (prefixSuffix & isPrefix) != 0
+	isS := (prefixSuffix & isSuffix) != 0
+
+	if isP && isS {
+		isP = rnd.ThrowWithProb(50)
+	}
+
+	if isP {
+		return value + " " + burg
+	}
+
+	return burg + " " + value
 }
